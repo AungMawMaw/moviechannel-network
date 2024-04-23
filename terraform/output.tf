d7f5787bd593cb9e403e48c6d96494f874d75bbb
AungMawMaw <aungmaw.bc@gmail.com>
2024-04-24 00:31:18 +0630

    test for terraform output

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,22 @@
 output "aws_region" {
   value = var.aws_region
 }
+
+# for subnet
+output "aws_subnet_id_public1" {
+  value = aws_subnet.public_1.id
+}
+output "aws_subnet_id_public2" {
+  value = aws_subnet.public_2.id
+}
+output "aws_subnet_id_private1" {
+  value = aws_subnet.private_1.id
+}
+output "aws_subnet_id_private2" {
+  value = aws_subnet.private_2.id
+}
+
+#vpc id
+output "aws_vpc_id" {
+  value = aws_vpc.vpc.id
+}
